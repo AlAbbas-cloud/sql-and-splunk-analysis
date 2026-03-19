79e6642b1f299b40890ef8a3d5d16d2d5d96c805
AlAbbas-cloud <mobilediagnosticcentre@outlook.com>
2026-03-19 18:22:21 +1000

    Create employee table with relevant fields

---
+++ sql/create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employee (
+    employee_no INT PRIMARY KEY,
+    name VARCHAR(50),
+    date_of_birth DATE,
+    position VARCHAR(50),
+    email VARCHAR(100),
+    phone_no VARCHAR(20)
+);
