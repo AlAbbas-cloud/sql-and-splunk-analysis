a60094cc386c77c716723700fa51e658e70c3c01
AlAbbas-cloud <mobilediagnosticcentre@outlook.com>
2026-03-19 18:32:19 +1000

    Update employee data for Michael

---
+++ sql/update_data.sql
@@ -0,0 +1,8 @@
+UPDATE employee
+SET employee_no = 1111,
+    name = 'Michael',
+    date_of_birth = '2000-01-01',
+    position = 'Salesman',
+    email = 'Michael.2000@certiv.come',
+    phone_no = '999'
+WHERE name = 'Michael';
