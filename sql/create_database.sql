c8ed0209d49078b58b1e5a2786d956933624d488
Ali Abbas <mobilediagnosticcentre@outlook.com>
2026-03-20 19:55:19 +1000

    Create employees database AliAbbas_employees_db

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -1,2 +1,4 @@
 -- Create employees database
 CREATE DATABASE AliAbbas_employees_db;
+
+![Create Database](https://raw.githubusercontent.com/AlAbbas-cloud/sql-and-splunk-analysis/refs/heads/main/sql/screenshots/Create%20Database.png)
